28964a023f715a96cafd208f325643027faf6d4f
SShifa <sifasasture@gmail.com>
2022-12-24 20:34:36 +0530

    hurry

--- src/com/terna/ArrayDemo.java
+++ src/com/terna/ArrayDemo.java
@@ -7,7 +7,8 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		ArrayList list =new ArrayList();
 		list.add("nikita");
-		list.add("mera");
+		list.add("dffgg");
+		
 		System.out.println(list);
 
 	}
